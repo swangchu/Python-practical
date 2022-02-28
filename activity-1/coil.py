43030f7da22463f3de6d481980febf08e2be8f97
Sonam Wangchu <swangchu79@gmail.com>
2022-02-28 11:44:36 +0600

    coil.py
    Using Turtle module

---
+++ activity-1/coil.py
@@ -0,0 +1,19 @@
+# Activity 1: coil.py
+# -----------------------------------------------------------------
+# Write a program using Turtle module to generate a coil
+# using five circle of radius 50 pixels and they are 25 pixels apath. 
+# ---------------------------------------------------------------------
+
+from turtle import *
+shape("turtle")
+
+circle(35)
+forward(20)
+circle(35)
+forward(20)
+circle(35)
+forward(20)
+circle(35)
+forward(20)
+circle(35)
+forward(20)
