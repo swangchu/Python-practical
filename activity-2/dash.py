e5dbfd74fc92e787b8c717e1772b39f1ebcb2dd0
Sonam Wangchu <swangchu79@gmail.com>
2022-02-28 12:28:53 +0600

    Create dash.py

---
+++ activity-2/dash.py
@@ -0,0 +1,34 @@
+# Activity 2: dash.py
+# -----------------------------------------------------------------
+# Write a program using Turtle module to generate five dash lines
+# starting 400 pixels from left of the center of the canvas and each dash to be
+# 100 pixels and they are 100 pixels apath.
+# ---------------------------------------------------------------------
+
+from turtle import *
+shape("turtle")
+
+penup()
+backward(400)
+pendown()
+forward(100)
+
+penup()
+forward(100)
+pendown()
+forward(100)
+
+penup()
+forward(100)
+pendown()
+forward(100)
+
+penup()
+forward(100)
+pendown()
+forward(100)
+
+penup()
+forward(100)
+pendown()
+forward(100)
