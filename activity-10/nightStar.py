91f71d25bc829bfeba51cd8fe169f9767d5a6523
Sonam Wangchu <swangchu79@gmail.com>
2022-02-28 17:57:19 +0600

    Create nightStar.py

---
+++ activity-10/nightStar.py
@@ -0,0 +1,30 @@
+# Activity 10: NightStar.py
+# -----------------------------------------------------------------
+# Write a program using Turtle module to generate a star with dark screen background.
+# ---------------------------------------------------------------------
+
+
+from turtle import *
+shape("turtle")
+
+speed(5)
+
+bgcolor("black")
+
+pensize(5)
+
+pencolor("white")
+
+forward(200)
+
+right(144)
+forward(200)
+
+right(144)
+forward(200)
+
+right(144)
+forward(200)
+
+right(144)
+forward(200)
