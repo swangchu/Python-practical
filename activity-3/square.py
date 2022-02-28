456fda8f238ba257739f547eaa3e2a126f3280c8
Sonam Wangchu <swangchu79@gmail.com>
2022-02-28 12:51:44 +0600

    Create square.py

---
+++ activity-3/square.py
@@ -0,0 +1,18 @@
+# Activity 3: square.py
+# -----------------------------------------------------------------
+# Write a program using Turtle module to generate a square of 100 pixels
+# ---------------------------------------------------------------------
+
+from turtle import *
+shape("turtle")
+
+forward(100)
+left(90)
+
+forward(100)
+left(90)
+
+forward(100)
+left(90)
+forward(100)
+
