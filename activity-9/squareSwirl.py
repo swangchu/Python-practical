80f1e43088c7e1dc9cea19d3bd3122b9801df87c
Sonam Wangchu <swangchu79@gmail.com>
2022-02-28 17:24:20 +0600

    Create squareSwirl.py

---
+++ activity-9/squareSwirl.py
@@ -0,0 +1,41 @@
+
+# Activity 9: squareSwirl.py
+# -----------------------------------------------------------------
+# Write a program using Turtle module to generate a square swirl
+# ---------------------------------------------------------------------
+
+
+from turtle import *
+shape("turtle")
+
+speed(5)
+
+forward(10)
+left(90)
+
+forward(20)
+left(90)
+
+forward(30)
+left(90)
+
+forward(40)
+left(90)
+
+forward(50)
+left(90)
+
+forward(60)
+left(90)
+
+forward(70)
+left(90)
+
+forward(80)
+left(90)
+
+forward(90)
+left(90)
+
+forward(100)
+left(90)
