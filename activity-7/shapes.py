ac421f684c3830ca528d841813ff17dfd7ffda4b
Sonam Wangchu <swangchu79@gmail.com>
2022-02-28 16:11:07 +0600

    Create shapes.py

---
+++ activity-7/shapes.py
@@ -0,0 +1,49 @@
+
+# Activity 7: shapes.py
+# -----------------------------------------------------------------
+# Write a program using Turtle module to generate series of shapes in line.
+# Begin with a triangle then pentagon, hexagon, heptagon, octagon, nonagon and decagon.
+# 
+# ---------------------------------------------------------------------
+
+
+from turtle import *
+shape("turtle")
+
+circle(25,360,3)
+pu()
+fd(50)
+
+pd()
+circle(25,360,5)
+pu()
+fd(50)
+
+pd()
+circle(25,360,6)
+pu()
+fd(50)
+
+pd()
+circle(25,360,7)
+pu()
+fd(50)
+
+pd()
+circle(25,360,8)
+pu()
+fd(50)
+
+pd()
+circle(25,360,9)
+pu()
+fd(50)
+
+pd()
+circle(25,360,10)
+pu()
+fd(50)
+
+
+
+
