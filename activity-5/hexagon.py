a9916cafdfe8c60c122158d8fdb7757f62d6b61b
Sonam Wangchu <swangchu79@gmail.com>
2022-02-28 14:24:22 +0600

    Create hexagon.py

---
+++ activity-5/hexagon.py
@@ -0,0 +1,44 @@
+
+# Activity 5: hexagon.py
+# -----------------------------------------------------------------
+# Write a program using Turtle module to generate a hexagon.
+# Each side of the hexagon should of different color and thinkcer than then default size.
+# Draw the hexagon with speed of 5.
+# 
+# ---------------------------------------------------------------------
+
+
+from turtle import *
+shape("turtle")
+
+speed(5)
+pensize(5)
+
+
+color("red")
+forward(50)
+left(60)
+
+color("orange")
+forward(50)
+left(60)
+
+color("yellow")
+forward(50)
+left(60)
+
+color("green")
+forward(50)
+left(60)
+
+color("blue")
+forward(50)
+left(60)
+
+color("indigo")
+forward(50)
+left(60)
+
+
+
+
