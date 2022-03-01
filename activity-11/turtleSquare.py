96046d489d3060ab89578aeda37a7c319a0796b6
Sonam Wangchu <swangchu79@gmail.com>
2022-03-01 12:28:23 +0600

    Create turtleSquare.py

---
+++ activity-11/turtleSquare.py
@@ -0,0 +1,34 @@
+# Activity 11: turtleSquare.py
+# -----------------------------------------------------------------
+# Write a program using Turtle to generate a square with one turtle only
+# on every corner of the square.
+# 
+# ---------------------------------------------------------------------
+
+
+from turtle import *
+shape("turtle")
+
+speed(5)
+
+
+stamp()
+
+fd(100)
+rt(90)
+
+stamp()
+
+fd(100)
+rt(90)
+
+stamp()
+
+fd(100)
+rt(90)
+
+stamp()
+
+fd(100)
+
+hideturtle()
